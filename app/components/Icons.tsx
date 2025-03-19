563fea8dc73cc1e4843f01a01ac4106dd6dbefdf
florianBKO <floriansavoie30@gmail.com>
2025-03-19 20:39:59 +0100

    Mise à jour de la configuration TypeScript et Tailwind

--- app/components/Icons.tsx
+++ app/components/Icons.tsx
@@ -10,7 +10,7 @@ export const LogoIcon = () => {
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
-      class="lucide lucide-notebook-text text-[#D247BF] me-2"
+      className="lucide lucide-notebook-text text-[#D247BF] me-2"
     >
       <path d="M2 6h4" />
       <path d="M2 10h4" />
@@ -641,5 +641,5 @@ export const MagnifierIcon = () => {
 };
 export const Bookmark = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-bookmark"><path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z"/></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-bookmark"><path d="m19 21-7-4-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16z"/></svg>
   )}
